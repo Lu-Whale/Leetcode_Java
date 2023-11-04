fea8716d0f464c2f31e0ba5de0aef89fc2093b30
Jingyu Lu <jilu2919@uni.sydney.edu.au>
2023-11-04 20:08:08 +1100

    11.4

---
+++ LeetCode75/Array_String/_345_Reverse_Vowels_of_a_String.java
@@ -0,0 +1,39 @@
+package LeetCode75.Array_String;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Stack;
+
+// https://leetcode.com/problems/reverse-vowels-of-a-string/?envType=study-plan-v2&envId=leetcode-75
+public class _345_Reverse_Vowels_of_a_String {
+    public static String reverseVowels(String s) {
+        HashSet<Character> hashSet = new HashSet<>(List.of('a','e','i','o','u','A','E','I','O','U'));
+
+        // add all vowels into stack
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(hashSet.contains(s.charAt(i))) {
+                stack.add(s.charAt(i));
+            }
+        }
+
+        //generate result string
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            if(!hashSet.contains(s.charAt(i))) {
+                stringBuilder.append(s.charAt(i));
+            }else {
+                stringBuilder.append(stack.pop().toString());
+            }
+        }
+        System.out.println(stringBuilder.toString());
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "hEllo";
+
+        reverseVowels(s);
+    }
+}
--- LeetCode75/Array_String/_605_Can_Place_Flowers.java
+++ LeetCode75/Array_String/_605_Can_Place_Flowers.java
@@ -1,7 +1,42 @@
 package LeetCode75.Array_String;
 
+// https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75
 public class _605_Can_Place_Flowers {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
 
+        int space = 0;
+
+        int boundary = flowerbed[0] == 0 ? 0 : 1;
+
+        for(int i = 0; i < flowerbed.length; i++) {
+
+            if(flowerbed[i] == 1) {
+                boundary++;
+                if(boundary == 2) {
+                    if(space >= 3) {
+                        int mod = space % 2;
+                        n -= mod == 0 ? space/2 - 1 : space/2;
+                    }
+
+                }else {
+                    n -= space/2;
+                }
+                space = 0;
+                if(boundary == 2) boundary--;
+            }else {
+                space++;
+            }
+        }
+
+        if(space != 0) {
+            if(boundary == 1) {
+                n -= space/2;
+            }else {
+                int mod = space % 2;
+                n -= mod == 0 ? space/2 : space/2 + 1;
+            }
+        }
+
+        return n <= 0;
     }
 }
