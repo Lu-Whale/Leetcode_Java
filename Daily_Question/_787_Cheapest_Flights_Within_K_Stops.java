5c56a5121f51bdd9022b4dac7dcd3f9ebb7dccc6
Jingyu Lu <jilu2919@uni.sydney.edu.au>
2024-03-02 22:57:40 +1100

    3.2

--- Daily_Question/_787_Cheapest_Flights_Within_K_Stops.java
+++ Daily_Question/_787_Cheapest_Flights_Within_K_Stops.java
@@ -152,3 +152,36 @@ class Solution_787_Dijkstra_withPathTracking {
         return -1;
     }
 }
+
+class Solution_787_Ford {
+    int N = 110, INF = Integer.MAX_VALUE;
+    int[][] g = new int[N][N];
+    int[] dist = new int[N];
+    int n, m, s, t, k;
+    public int findCheapestPrice(int _n, int[][] flights, int _src, int _dst, int _k) {
+        n = _n; s = _src; t = _dst; k = _k + 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                g[i][j] = i == j ? 0 : INF;
+            }
+        }
+        for (int[] f : flights) {
+            g[f[0]][f[1]] = f[2];
+        }
+        int ans = Bellman_Ford();
+        return ans > INF / 2 ? -1 : ans;
+    }
+    public int Bellman_Ford() {
+        Arrays.fill(dist, INF);
+        dist[s] = 0;
+        for (int limit = 0; limit < k; limit++) {
+            int[] clone = dist.clone();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    dist[j] = Math.min(dist[j], clone[i] + g[i][j]);
+                }
+            }
+        }
+        return dist[t];
+    }
+}
