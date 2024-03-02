841e6e48c8658a6d99d75848d793659c70f6fb9b
Jingyu Lu <jilu2919@uni.sydney.edu.au>
2024-03-02 22:57:25 +1100

    3.2

---
+++ Daily_Question/_977_Squares_of_a_Sorted_Array.java
@@ -0,0 +1,30 @@
+package Daily_Question;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+// 3.2
+public class _977_Squares_of_a_Sorted_Array {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int lp = 0;
+        int rp = n - 1;
+
+        int[] res = new int[n];
+        int index = n - 1;
+        while (lp <= rp) {
+            int squareLp = nums[lp] * nums[lp];
+            int squareRp = nums[rp] * nums[rp];
+            if(squareRp >= squareLp) {
+                res[index] = squareRp;
+                rp--;
+            }else {
+                res[index] = squareLp;
+                lp++;
+            }
+            index--;
+        }
+        return res;
+    }
+}
