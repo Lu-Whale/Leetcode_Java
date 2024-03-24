680ec2b2dd3c84ee103589e2da13fe64d1d6bd7d
jilu2919@uni.sydney.edu.au Lucas Lu <lujingyu2919@gmail.com>
2024-03-24 21:03:10 +1100

    3.24

--- Daily_Question/_287_Find_the_Duplicate_Number.java
+++ Daily_Question/_287_Find_the_Duplicate_Number.java
@@ -5,6 +5,9 @@ public class _287_Find_the_Duplicate_Number {
         for(int i=0;i<nums.length;i++) {
             int ind = Math.abs(nums[i]);
             if(nums[ind] < 0) {
+                for(int j = 0; j < nums.length; j++) {
+                    nums[j] = Math.abs(nums[j]);
+                }
                 return ind;
             }
             nums[ind] = -nums[ind];
