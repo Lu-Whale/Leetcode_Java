4a467633a43e7ed26359e4059a5c6d1cc5e1edfd
Lucas Lu <lujingyu2919@gmail.com>
2024-03-27 00:50:31 +1100

    3.26

---
+++ Daily_Question/_41_First_Missing_Positive.java
@@ -0,0 +1,37 @@
+package Daily_Question;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class _41_First_Missing_Positive {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // first, we pick a meaningless number used as flag
+        // in this problem, we aimed to find the positive number, so just use 0
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 0) nums[i] = -1;
+        }
+
+        for(int i = 0; i < n; i++) {
+            int num = nums[i];
+            if(num > 0 && num <= n) {
+                dfs(nums, num - 1);
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != 0) return i+1;
+        }
+
+        return n+1;
+    }
+
+    public void dfs(int[] nums, int index) {
+        int num = nums[index];
+        nums[index] = 0;
+        if(num > 0 && num <= nums.length) {
+            dfs(nums, num - 1);
+        }
+    }
+}
