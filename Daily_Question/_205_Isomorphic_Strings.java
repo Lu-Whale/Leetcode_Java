5a6eb2cef67ba5b262565f2cca783877d3a2e384
Lucas Lu <lujingyu2919@gmail.com>
2024-04-02 19:33:02 +1100

    4.1

---
+++ Daily_Question/_205_Isomorphic_Strings.java
@@ -0,0 +1,27 @@
+package Daily_Question;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class _205_Isomorphic_Strings {
+    public boolean isIsomorphic(String s, String t) {
+        // key -> cs, value -> ct
+        HashMap<Character, Character> hashMap = new HashMap<>();
+        HashSet<Character> hashSet = new HashSet<>();
+        for(int i = 0; i < s.length(); i++) {
+            char cs = s.charAt(i);
+            char ct = t.charAt(i);
+            if(!hashMap.containsKey(cs)) {
+                if(hashSet.contains(ct)) return false;
+                hashSet.add(ct);
+                hashMap.put(cs, ct);
+            }else {
+                if(hashMap.get(cs) != ct) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
