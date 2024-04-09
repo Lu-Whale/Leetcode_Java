1ba91185696b2951ad4ea3a1dc859eebd7391b66
Lucas Lu <lujingyu2919@gmail.com>
2024-04-09 17:21:24 +1000

    4.9

---
+++ Daily_Question/_2073_Time_Needed_to_Buy_Tickets.java
@@ -0,0 +1,16 @@
+package Daily_Question;
+
+public class _2073_Time_Needed_to_Buy_Tickets {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int res = 0;
+        for(int i = 0; i < tickets.length; i++) {
+            if(i <= k) {
+                res += Math.min(tickets[i], tickets[k]);
+            }else {
+                res += Math.min(tickets[i], tickets[k] - 1);
+            }
+        }
+
+        return res;
+    }
+}
