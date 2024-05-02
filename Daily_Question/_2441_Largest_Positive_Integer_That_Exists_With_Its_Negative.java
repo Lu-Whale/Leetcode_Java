a5400bbaec52a816090b859bd578ea0cf88bc591
Lucas Lu <lujingyu2919@gmail.com>
2024-05-02 17:11:00 +1000

    5.2

---
+++ Daily_Question/_2441_Largest_Positive_Integer_That_Exists_With_Its_Negative.java
@@ -0,0 +1,21 @@
+package Daily_Question;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class _2441_Largest_Positive_Integer_That_Exists_With_Its_Negative {
+    public int findMaxK(int[] nums) {
+        int res = -1;
+
+        HashSet<Integer> hashSet = new HashSet<>();
+        for(int num : nums) {
+            if(hashSet.contains(-num)) {
+                res = Math.max(Math.abs(num), res);
+            }else {
+                hashSet.add(num);
+            }
+        }
+
+        return res;
+    }
+}
